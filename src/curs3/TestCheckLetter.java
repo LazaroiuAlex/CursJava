72f827bcd5152ba3eb55f391972b0f775cada75c
unknown <lazaroiu.alex03@gmail.com>
2024-03-11 10:33:20 +0200

    curs 3 intermediar

---
+++ src/curs3/TestCheckLetter.java
@@ -0,0 +1,12 @@
+package curs3;
+
+public class TestCheckLetter {
+
+	public static void main(String[] args) {
+		
+		CheckLetter obj = new CheckLetter();
+		obj.askTheUserForACharacter();
+		obj.checkIfCharacterIsLetter();
+	}
+
+}
