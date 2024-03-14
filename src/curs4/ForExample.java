8db5123f879c812a27fe8ec567ffd85336c95c79
unknown <lazaroiu.alex03@gmail.com>
2024-03-14 11:27:31 +0200

    Curs 4 final

---
+++ src/curs4/ForExample.java
@@ -0,0 +1,22 @@
+package curs4;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		
+		//initializare, conditie, iterator
+		for(int i=0; i<10; i++) { //stai in bucla cat i este mai mic ca 10 -- i++ iterator
+			System.out.println("Valoarea lui i este " +i);
+		}
+		System.out.println("--------------------------------------------");
+		for(int i=0; i<10; i++) {
+			if(i == 5) {
+				//break;//intrerupe executia buclei
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
