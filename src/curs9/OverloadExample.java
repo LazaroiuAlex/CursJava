a144ce5266cab0dcc9efefe3c88f41540d2b7cc7
unknown <lazaroiu.alex03@gmail.com>
2024-04-01 10:40:15 +0300

    curs9 intermediar

---
+++ src/curs9/OverloadExample.java
@@ -0,0 +1,15 @@
+package curs9;
+
+public class OverloadExample {
+
+	public static void main(String[] args) {
+
+		System.out.println(multiply(2, 3));
+
+	}
+	
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
+
+}
---
+++ src/curs9/WildAnimal.java
@@ -0,0 +1,18 @@
+package curs9;
+
+public class WildAnimal {
+	
+	public void makeSound() {
+		
+		System.out.println("Make an animal sound!");
+		
+	}
+	
+	public void whereDoesItLive() {
+		
+		System.out.println("Wilderness");
+		
+	}
+	
+
+}
